856d04308d0dabf2f0137a26ea97ee5d40c0e3af
Nagendra <nagendra.singh@iqvia.com>
2021-07-18 17:01:32 +0530

    .

--- src/main/java/com/example/herokuconnect/RestApiPostGres.java
+++ src/main/java/com/example/herokuconnect/RestApiPostGres.java
@@ -3,6 +3,7 @@ package com.example.herokuconnect;
 import com.example.herokuconnect.model.Contact;
 import com.example.herokuconnect.repository.ContactRepository;
 import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.*;
@@ -13,11 +14,9 @@ import java.util.List;
 @RestController
 public class RestApiPostGres {
 
-	final ContactRepository contactRepository;
+	@Autowired
+	ContactRepository contactRepository;
 
-	public RestApiPostGres(ContactRepository contactRepository) {
-		this.contactRepository = contactRepository;
-	}
 
 	@GetMapping(path = "/fetchAllContacts")
 	public Flux<Contact> fetchAllContacts(){
