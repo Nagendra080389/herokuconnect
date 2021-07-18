8fab887a2c5d6c67784e082bed5769f914b97d17
Nagendra <nagendra.singh@iqvia.com>
2021-07-18 13:29:36 +0530

    .

--- src/main/java/com/example/herokuconnect/model/Contact.java
+++ src/main/java/com/example/herokuconnect/model/Contact.java
@@ -1,78 +1,46 @@
 package com.example.herokuconnect.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Contact {
 
-	private Long Id;
-	private String FirstName;
-	private String LastName;
-	private String MiddleName;
-	private String Name;
+	private Long sfid;
+	private String firstname;
+	private String lastname;
+	private String name;
 
-	@javax.persistence.Id
-	public Long getId() {
-		return Id;
+	@Id
+	public Long getSfid() {
+		return sfid;
 	}
 
-	public void setId(Long id) {
-		Id = id;
+	public void setSfid(Long sfid) {
+		this.sfid = sfid;
 	}
 
-	public String getFirstName() {
-		return FirstName;
+	public String getFirstname() {
+		return firstname;
 	}
 
-	public void setFirstName(String firstName) {
-		FirstName = firstName;
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
 	}
 
-	public String getLastName() {
-		return LastName;
+	public String getLastname() {
+		return lastname;
 	}
 
-	public void setLastName(String lastName) {
-		LastName = lastName;
-	}
-
-	public String getMiddleName() {
-		return MiddleName;
-	}
-
-	public void setMiddleName(String middleName) {
-		MiddleName = middleName;
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
 	}
 
 	public String getName() {
-		return Name;
+		return name;
 	}
 
 	public void setName(String name) {
-		Name = name;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-
-		Contact contact = (Contact) o;
-
-		if (Id != null ? !Id.equals(contact.Id) : contact.Id != null) return false;
-		if (FirstName != null ? !FirstName.equals(contact.FirstName) : contact.FirstName != null) return false;
-		if (LastName != null ? !LastName.equals(contact.LastName) : contact.LastName != null) return false;
-		if (MiddleName != null ? !MiddleName.equals(contact.MiddleName) : contact.MiddleName != null) return false;
-		return Name != null ? Name.equals(contact.Name) : contact.Name == null;
-	}
-
-	@Override
-	public int hashCode() {
-		int result = Id != null ? Id.hashCode() : 0;
-		result = 31 * result + (FirstName != null ? FirstName.hashCode() : 0);
-		result = 31 * result + (LastName != null ? LastName.hashCode() : 0);
-		result = 31 * result + (MiddleName != null ? MiddleName.hashCode() : 0);
-		result = 31 * result + (Name != null ? Name.hashCode() : 0);
-		return result;
+		this.name = name;
 	}
 }
